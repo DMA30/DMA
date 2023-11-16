0aa132332131b5051b3878439ff6896fb1777c95
dmihailov <dmihailov@s-terra.ru>
2023-11-16 18:10:17 +0300

    #hw03_chessboard
    +go.mod
    +go.sum
    +hw03_chessboard
    +main.go - рефакторинг

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,30 @@
 package main
 
+import "fmt"
+
 func main() {
-	// Place your code here.
+	empty := ""
+	symbol := "#"
+	space := " "
+	a := 8
+	b := 8
+	for i := 0; i < a; i++ {
+		for n := 0; n < b; n++ {
+			if i%2 == 0 {
+				if n%2 == 0 {
+					empty = symbol
+				} else {
+					empty = space
+				}
+			} else {
+				if n%2 == 0 {
+					empty = space
+				} else {
+					empty = symbol
+				}
+			}
+			fmt.Print(empty)
+		}
+		fmt.Println()
+	}
 }
