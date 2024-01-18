6651bb80c21f924ef3a1c909a9ffd9f3ba6f20d9
dmihailov <dmihailov@s-terra.ru>
2024-01-18 14:31:40 +0300

    (#hw04_struct_comparator) Компаратор структур
    +go.mod
    +go.sum
    +hw04_struct_comparator
    +main.go - реализована структура Book с возможностью сравнивания полей

--- hw04_struct_comparator/main.go
+++ hw04_struct_comparator/main.go
@@ -1,5 +1,69 @@
 package main
 
+import (
+	"fmt"
+)
+
+type Event float64
+
+const (
+	Year Event = iota
+	Size
+	Rate
+)
+
+type Book struct {
+	id     string
+	title  string
+	author string
+	year   int
+	size   int
+	rate   float64
+}
+
+func NewBook(event Event, id, title, author string, year, size int, rate float64) *Book {
+	return &Book{
+		id:     id,
+		title:  title,
+		author: author,
+		year:   year,
+		size:   size,
+		rate:   rate,
+	}
+}
+
+func (b *Book) IsComparison(other *Book, event Event) bool {
+	switch event {
+	case Year:
+		return b.year > other.year
+	case Size:
+		return b.size > other.size
+	case Rate:
+		return b.rate > other.rate
+	}
+	return false
+
+}
+
 func main() {
-	// Place your code here.
+	book := NewBook(Year, "0", "toxic", "DMA", 1999, 1200, 4.6)
+	book1 := NewBook(Size, "1", "toxic", "DMA", 2000, 1000, 4.0)
+
+	if book.IsComparison(book1, Year) {
+		fmt.Println("book is bigger than book1")
+	} else {
+		fmt.Println("book is smaller than book1")
+	}
+
+	if book.IsComparison(book1, Size) {
+		fmt.Println("book is bigger than book1")
+	} else {
+		fmt.Println("book is smaller than book1")
+	}
+
+	if book.IsComparison(book1, Rate) {
+		fmt.Println("book is bigger than book1")
+	} else {
+		fmt.Println("book is smaller than book1")
+	}
 }
