8aee17185c027c7e3c7ad1b54bdd99428ac8a58b
Dmitry Mikhailov <dimchismex@mail.ru>
2024-01-22 14:55:25 +0300

    (refs #newHw04_struct_comparator) - Компаратор структур
    +main.go - рефакторинг

--- hw04_struct_comparator/main.go
+++ hw04_struct_comparator/main.go
@@ -13,57 +13,76 @@ const (
 )
 
 type Book struct {
-	id     string
-	title  string
-	author string
-	year   int
-	size   int
-	rate   float64
+	compareType Event
+	id          string
+	title       string
+	author      string
+	year        int
+	size        int
+	rate        float64
 }
 
 func NewBook(event Event, id, title, author string, year, size int, rate float64) *Book {
 	return &Book{
-		id:     id,
-		title:  title,
-		author: author,
-		year:   year,
-		size:   size,
-		rate:   rate,
+		id:          id,
+		title:       title,
+		author:      author,
+		year:        year,
+		size:        size,
+		rate:        rate,
+		compareType: event,
 	}
 }
 
-func (b *Book) IsComparison(other *Book, event Event) bool {
-	switch event {
+func (b *Book) IsComparison(other *Book) int {
+	switch b.compareType {
 	case Year:
-		return b.year > other.year
+		return compareYears(b, other)
 	case Size:
-		return b.size > other.size
+		return compareSizes(b, other)
 	case Rate:
-		return b.rate > other.rate
+		return compareRates(b, other)
 	}
-	return false
+	return 0
+}
 
+func compareYears(b, other *Book) int {
+	if b.year == other.year {
+		return 0
+	} else if b.year > other.year {
+		return 1
+	}
+	return -1
+}
+
+func compareSizes(b, other *Book) int {
+	if b.size == other.size {
+		return 0
+	} else if b.size > other.size {
+		return 1
+	}
+	return -1
+}
+
+func compareRates(b, other *Book) int {
+	if b.rate == other.rate {
+		return 0
+	} else if b.rate > other.rate {
+		return 1
+	}
+	return -1
 }
 
 func main() {
-	book := NewBook(Year, "0", "toxic", "DMA", 1999, 1200, 4.6)
-	book1 := NewBook(Size, "1", "toxic", "DMA", 2000, 1000, 4.0)
+	book := NewBook(Year, "0", "toxic", "DMA", 2000, 1200, 4.1)
+	book1 := NewBook(Size, "1", "toxic", "DMA", 2000, 1200, 4.6)
 
-	if book.IsComparison(book1, Year) {
-		fmt.Println("book is bigger than book1")
-	} else {
-		fmt.Println("book is smaller than book1")
-	}
-
-	if book.IsComparison(book1, Size) {
-		fmt.Println("book is bigger than book1")
-	} else {
-		fmt.Println("book is smaller than book1")
-	}
-
-	if book.IsComparison(book1, Rate) {
-		fmt.Println("book is bigger than book1")
-	} else {
-		fmt.Println("book is smaller than book1")
+	switch book.IsComparison(book1) {
+	case 0:
+		fmt.Println("Book equality")
+	case 1:
+		fmt.Println("book greater book1")
+	case -1:
+		fmt.Println("book smaller book1")
 	}
 }
