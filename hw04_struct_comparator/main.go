1d6276a9df3b26039fc0bfa67d980ce64b72db00
dmihailov <dmihailov@s-terra.ru>
2024-02-13 16:05:40 +0300

    (refs hw04_struct_comparator) Компаратор структур
    *main.go - исправлены ошибки

--- hw04_struct_comparator/main.go
+++ hw04_struct_comparator/main.go
@@ -4,85 +4,134 @@ import (
 	"fmt"
 )
 
-type Event float64
+type Book struct {
+	ID     int
+	Title  string
+	Author string
+	Year   int
+	Size   int
+	Rate   float64
+}
+
+func (b *Book) SetID(id int) {
+	b.ID = id
+}
+
+func (b *Book) GetID() int {
+	return b.ID
+}
+
+func (b *Book) SetTitle(title string) {
+	b.Title = title
+}
+
+func (b *Book) GetTitle() string {
+	return b.Title
+}
+
+func (b *Book) SetAuthor(author string) {
+	b.Author = author
+}
+
+func (b *Book) GetAuthor() string {
+	return b.Author
+}
+
+func (b *Book) SetYear(year int) {
+	b.Year = year
+}
+
+func (b *Book) GetYear() int {
+	return b.Year
+}
+
+func (b *Book) SetSize(size int) {
+	b.Size = size
+}
+
+func (b *Book) GetSize() int {
+	return b.Size
+}
+
+func (b *Book) SetRate(rate float64) {
+	b.Rate = rate
+}
+
+func (b *Book) GetRate() float64 {
+	return b.Rate
+}
+
+type BookComparatorMode int
 
 const (
-	Year Event = iota
+	Year BookComparatorMode = iota
 	Size
 	Rate
 )
 
-type Book struct {
-	compareType Event
-	id          string
-	title       string
-	author      string
-	year        int
-	size        int
-	rate        float64
+type BookComparator struct {
+	mode BookComparatorMode
 }
 
-func NewBook(event Event, id, title, author string, year, size int, rate float64) *Book {
-	return &Book{
-		id:          id,
-		title:       title,
-		author:      author,
-		year:        year,
-		size:        size,
-		rate:        rate,
-		compareType: event,
-	}
+func NewBookComparator(mode BookComparatorMode) BookComparator {
+	return BookComparator{mode}
 }
 
-func (b *Book) IsComparison(other *Book) int {
-	switch b.compareType {
+func (c *BookComparator) Compare(book1, book2 Book) int {
+	switch c.mode {
 	case Year:
-		return compareYears(b, other)
+		if book1.Year < book2.Year {
+			return -1
+		} else if book1.Year > book2.Year {
+			return 1
+		}
 	case Size:
-		return compareSizes(b, other)
+		if book1.Size < book2.Size {
+			return -1
+		} else if book1.Size > book2.Size {
+			return 1
+		}
 	case Rate:
-		return compareRates(b, other)
+		if book1.Rate < book2.Rate {
+			return -1
+		} else if book1.Rate > book2.Rate {
+			return 1
+		}
 	}
 	return 0
 }
 
-func compareYears(b, other *Book) int {
-	if b.year == other.year {
-		return 0
-	} else if b.year > other.year {
-		return 1
-	}
-	return -1
-}
-
-func compareSizes(b, other *Book) int {
-	if b.size == other.size {
-		return 0
-	} else if b.size > other.size {
-		return 1
-	}
-	return -1
-}
-
-func compareRates(b, other *Book) int {
-	if b.rate == other.rate {
-		return 0
-	} else if b.rate > other.rate {
-		return 1
-	}
-	return -1
-}
-
 func main() {
-	book := NewBook(Year, "0", "toxic", "DMA", 2000, 1200, 4.1)
-	book1 := NewBook(Size, "1", "toxic", "DMA", 2000, 1200, 4.6)
+	book1 := Book{ID: 1, Title: "Book 1", Author: "Author 1", Year: 2020, Size: 200, Rate: 4.5}
+	book2 := Book{ID: 2, Title: "Book 2", Author: "Author 2", Year: 2020, Size: 200, Rate: 4.5}
 
-	switch book.IsComparison(book1) {
-	case 0:
-		fmt.Println("Book equality")
-	case 1:
-		fmt.Println("book greater book1")
+	comparatorByYear := NewBookComparator(Year)
+	comparatorBySize := NewBookComparator(Size)
+	comparatorByRate := NewBookComparator(Rate)
+
+	switch comparatorByYear.Compare(book1, book2) {
 	case -1:
-		fmt.Println("book smaller book1")
+		fmt.Println("Book 1 is older than Book 2")
+	case 1:
+		fmt.Println("Book 2 is older than Book 1")
+	case 0:
+		fmt.Println("Book 1 and Book 2 are identical")
+	}
+	switch comparatorBySize.Compare(book1, book2) {
+	case -1:
+		fmt.Println("Book 1 is smaller than Book 2")
+	case 1:
+		fmt.Println("Book 2 is smaller than Book 1")
+	case 0:
+		fmt.Println("Book 1 and Book 2 are identical")
+	}
+
+	switch comparatorByRate.Compare(book1, book2) {
+	case -1:
+		fmt.Println("Book 1 is cheaper than Book 2")
+	case 1:
+		fmt.Println("Book 2 is cheaper than Book 1")
+	case 0:
+		fmt.Println("Book 1 and Book 2 are identical")
 	}
 }
